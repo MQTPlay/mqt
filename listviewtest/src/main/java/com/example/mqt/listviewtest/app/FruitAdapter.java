7138a3d49c2ac0c8b52807dfb831549612546901
MQTStudio <brucelee919@gmail.com>
2015-06-01 15:52:49 +0800

    add new

--- listviewtest/src/main/java/com/example/mqt/listviewtest/app/FruitAdapter.java
+++ listviewtest/src/main/java/com/example/mqt/listviewtest/app/FruitAdapter.java
@@ -26,6 +26,8 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
         Fruit fruit = getItem(position);
         // add logic to change to convertView
         View view;
+
+/*        //comment for improve performance
         if (convertView == null) {
             view = LayoutInflater.from(getContext()).inflate(resourceId, null);
         } else {
@@ -36,6 +38,27 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
         TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
         fruitImage.setImageResource(fruit.getImageId());
         fruitName.setText(fruit.getName());
+*/
+
+        // added for improve list performance
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder); //store the viewHolder into the View
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+    class ViewHolder
+    {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
