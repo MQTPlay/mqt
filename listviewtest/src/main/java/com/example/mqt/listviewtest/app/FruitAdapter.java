3926f73748429b3d97fabd01bbb63be9b09813b7
MQTStudio <brucelee919@gmail.com>
2015-06-01 15:20:13 +0800

    Initial commit

---
+++ listviewtest/src/main/java/com/example/mqt/listviewtest/app/FruitAdapter.java
@@ -0,0 +1,41 @@
+package com.example.mqt.listviewtest.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by bruce_000 on 1/6/2015.
+ */
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceId;
+
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        // add logic to change to convertView
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+        } else {
+            view = convertView;
+        }
+//        View view = LayoutInflater.from(getContext()).inflate(resourceId, null);  --cache the view to the convertView
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+        return view;
+    }
+}
